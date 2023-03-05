3dd16973bda5ee8c1fdc41b6da7d46b050d1c91f
ferisigitpratama <84336440+ferisigitpratama@users.noreply.github.com>
2023-03-06 00:42:10 +0700

    Delete login.php

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    if ($username == 'masbro' && $password == '32022017') {
-        echo "Congrats!, Account has been hacked!";
-    } else {
-        echo "Haha! you failed!";
-    }
-?>
